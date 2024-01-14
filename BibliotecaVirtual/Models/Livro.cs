f693488914d78c3f654f5689e7d6f761bdeee3af
RandolfoCarvalho <randolfoirades@gmail.com>
2024-01-14 14:03:54 -0300

    Atualiza a validacao no form de criacao

--- BibliotecaVirtual/Models/Livro.cs
+++ BibliotecaVirtual/Models/Livro.cs
@@ -1,19 +1,25 @@
 ﻿using BibliotecaVirtual.Enums;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BibliotecaVirtual.Models
 {
     public class Livro
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O Nome é obrigatório")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "O {0} é obrigatório")]
         public string Genero { get; set; }
+        [Required(ErrorMessage = "O {0} é obrigatório")]
         [DisplayName("Ano de publicação")]
         public DateTime AnoPublicacao { get; set; }
+        [Required(ErrorMessage = "O {0} é obrigatório")]
         [DisplayName("Numero de páginas")]
         public int NumeroPaginas { get; set; }
-
+        [Required(ErrorMessage = "A {0} é obrigatória")]
         public string Sinopse { get; set; }
+        [Required(ErrorMessage = "A {0} é obrigatória")]
         public DisponibilidadeLivro Disponibilidade { get; set; }
 
     }
